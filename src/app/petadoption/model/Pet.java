104de80d8405345bfbf017fce00f3fd0a9e79235
AkumaYin0908 <buzterblaze0908@gmail.com>
2023-04-29 15:52:47 +0800

    initiate project

---
+++ src/app/petadoption/model/Pet.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app.petadoption.model;
+
+/**
+ *
+ * @author User
+ */
+public class Pet {
+    private int petId;
+    private String name;
+    private int age;
+}
